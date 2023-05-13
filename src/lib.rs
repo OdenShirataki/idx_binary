903484b8ffe560385b7672fb4c2de86f55fd9cd7
Oden <oden.srtk@gmail.com>
2023-05-13 13:48:12 +0900

    refactor!:abstraction
    delete Entity and num

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,34 @@
-use std::{cmp::Ordering, io, path::Path};
+use std::{
+    cmp::Ordering,
+    io,
+    ops::{Deref, DerefMut},
+    path::Path,
+};
 
 pub use natord;
 
 use anyhow::Result;
 pub use idx_file::{anyhow, Avltriee, AvltrieeHolder, AvltrieeIter, FileMmap, Found, IdxFile};
-use various_data_file::{DataAddress, VariousDataFile};
+pub use various_data_file::DataAddress;
+use various_data_file::VariousDataFile;
 
-#[derive(Clone, PartialEq, Debug)]
-pub struct Entity {
-    data_address: DataAddress,
-    num: f64,
-}
-impl Entity {
-    pub fn new(data_address: &DataAddress, num: f64) -> Self {
-        Self {
-            data_address: data_address.clone(),
-            num,
-        }
-    }
-    pub fn data_address(&self) -> &DataAddress {
-        &self.data_address
-    }
-    pub fn num(&self) -> f64 {
-        self.num
-    }
+pub trait DataAddressHolder<T> {
+    fn data_address(&self) -> &DataAddress;
+    fn new(data_address: DataAddress, input: &[u8]) -> T;
 }
 
-pub struct IdxBinary {
-    index: IdxFile<Entity>,
+pub struct IdxBinary<T> {
+    index: IdxFile<T>,
     data_file: VariousDataFile,
 }
-impl AvltrieeHolder<Entity, &[u8]> for IdxBinary {
-    fn triee(&self) -> &Avltriee<Entity> {
-        self.triee()
+impl<T: DataAddressHolder<T>> AvltrieeHolder<T, &[u8]> for IdxBinary<T> {
+    fn triee(&self) -> &Avltriee<T> {
+        self.index.triee()
     }
-    fn triee_mut(&mut self) -> &mut Avltriee<Entity> {
+    fn triee_mut(&mut self) -> &mut Avltriee<T> {
         self.index.triee_mut()
     }
-    fn cmp(&self, left: &Entity, right: &&[u8]) -> Ordering {
+    fn cmp(&self, left: &T, right: &&[u8]) -> Ordering {
         self.cmp(left, right)
     }
 
@@ -45,17 +36,14 @@ impl AvltrieeHolder<Entity, &[u8]> for IdxBinary {
         self.index.triee().search_end(|data| self.cmp(data, input))
     }
 
-    fn value(&mut self, input: &[u8]) -> Result<Entity> {
-        let data_address = self.data_file.insert(input)?;
-        Ok(Entity::new(
-            data_address.address(),
-            unsafe { std::str::from_utf8_unchecked(input) }
-                .parse()
-                .unwrap_or(0.0),
+    fn value(&mut self, input: &[u8]) -> Result<T> {
+        Ok(T::new(
+            self.data_file.insert(input)?.address().clone(),
+            input,
         ))
     }
 
-    fn delete(&mut self, row: u32, delete_node: &Entity) -> Result<()> {
+    fn delete(&mut self, row: u32, delete_node: &T) -> Result<()> {
         if !unsafe { self.index.triee().has_same(row) } {
             self.data_file.delete(&delete_node.data_address()).unwrap();
         }
@@ -64,7 +52,19 @@ impl AvltrieeHolder<Entity, &[u8]> for IdxBinary {
     }
 }
 
-impl IdxBinary {
+impl<T> Deref for IdxBinary<T> {
+    type Target = IdxFile<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.index
+    }
+}
+impl<T> DerefMut for IdxBinary<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.index
+    }
+}
+
+impl<T: DataAddressHolder<T>> IdxBinary<T> {
     pub fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
         let path = path.as_ref();
         Ok(Self {
@@ -80,38 +80,25 @@ impl IdxBinary {
             })?,
         })
     }
-
-    pub fn triee(&self) -> &Avltriee<Entity> {
-        self.index.triee()
-    }
-
-    pub fn get(&self, row: u32) -> Option<&'static [u8]> {
-        if let Some(e) = self.index.value(row) {
-            Some(unsafe { self.data_file.bytes(e.data_address()) })
+    pub fn bytes(&self, row: u32) -> Option<&'static [u8]> {
+        if let Some(value) = self.index.value(row) {
+            Some(unsafe { self.data_file.bytes(&value.data_address()) })
         } else {
             None
         }
     }
-    pub fn num(&self, row: u32) -> Option<f64> {
-        if let Some(e) = self.index.value(row) {
-            Some(e.num())
-        } else {
-            None
-        }
-    }
-    pub fn update(&mut self, row: u32, content: &[u8]) -> Result<u32> {
+
+    pub fn update(&mut self, row: u32, content: &[u8]) -> Result<u32>
+    where
+        T: Clone,
+    {
         let row = self.index.new_row(row)?;
         unsafe {
             Avltriee::update_holder(self, row, content)?;
         }
         Ok(row)
     }
-    pub fn delete(&mut self, row: u32) -> std::io::Result<()> {
-        self.index.delete(row)?;
-        Ok(())
-    }
-
-    pub fn cmp(&self, data: &Entity, content: &[u8]) -> Ordering {
+    pub fn cmp(&self, data: &T, content: &[u8]) -> Ordering {
         let left = unsafe { self.data_file.bytes(data.data_address()) };
         if left == content {
             Ordering::Equal
