cf5b93e1b459b3ade143f9504dbe19b329372738
OdenShirataki <oden.srtk@gmail.com>
2024-02-06 21:29:34 +0900

    refactor!: update idx_file and merge from binary_set

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
 mod compare;
 
-pub use compare::compare;
-pub use idx_file::{Avltriee, AvltrieeIter, AvltrieeUpdate, FileMmap, Found, IdxFile};
-use idx_file::{AvltrieeOrd, IdxFileAllocator};
-pub use various_data_file::DataAddress;
+use std::{cmp::Ordering, num::NonZeroU32, path::Path};
 
-use std::{
-    cmp::Ordering,
-    num::NonZeroU32,
-    ops::{Deref, DerefMut},
-    path::Path,
+pub use compare::compare;
+pub use idx_file::{
+    AvltrieeIter, AvltrieeSearch, AvltrieeUpdate, FileMmap, IdxFile, IdxFileAvlTriee,
 };
 
-use various_data_file::VariousDataFile;
+use idx_file::{search, IdxFileAllocator};
+use various_data_file::{DataAddress, VariousDataFile};
 
+type IdxBinaryAvltriee = IdxFileAvlTriee<DataAddress, [u8]>;
 type IdxBinaryAllocator = IdxFileAllocator<DataAddress>;
 
 pub struct IdxBinary {
@@ -21,37 +18,30 @@ pub struct IdxBinary {
     data_file: VariousDataFile,
 }
 
-impl Deref for IdxBinary {
-    type Target = IdxFile<DataAddress, [u8]>;
-    fn deref(&self) -> &Self::Target {
+impl AsRef<IdxBinaryAvltriee> for IdxBinary {
+    fn as_ref(&self) -> &IdxBinaryAvltriee {
         &self.index
     }
 }
-impl DerefMut for IdxBinary {
-    fn deref_mut(&mut self) -> &mut Self::Target {
+
+impl AsMut<IdxBinaryAvltriee> for IdxBinary {
+    fn as_mut(&mut self) -> &mut IdxBinaryAvltriee {
         &mut self.index
     }
 }
 
-impl AsRef<Avltriee<DataAddress, [u8], IdxBinaryAllocator>> for IdxBinary {
-    fn as_ref(&self) -> &Avltriee<DataAddress, [u8], IdxBinaryAllocator> {
-        self
-    }
-}
-impl AsMut<Avltriee<DataAddress, [u8], IdxBinaryAllocator>> for IdxBinary {
-    fn as_mut(&mut self) -> &mut Avltriee<DataAddress, [u8], IdxBinaryAllocator> {
-        self
-    }
-}
-
-impl AvltrieeOrd<DataAddress, [u8], IdxBinaryAllocator> for IdxBinary {
+impl AvltrieeSearch<DataAddress, [u8], IdxBinaryAllocator> for IdxBinary {
     fn cmp(&self, left: &DataAddress, right: &[u8]) -> Ordering {
         compare(self.data_file.bytes(left), right)
     }
+
+    fn convert<'a, 'b: 'a>(&'a self, value: &'b DataAddress) -> &[u8] {
+        self.data_file.bytes(value)
+    }
 }
 
-impl AvltrieeUpdate<DataAddress, [u8], IdxFileAllocator<DataAddress>> for IdxBinary {
-    fn unique_value(&mut self, input: &[u8]) -> DataAddress {
+impl AvltrieeUpdate<DataAddress, [u8], IdxBinaryAllocator> for IdxBinary {
+    fn convert_value_on_insert_unique(&mut self, input: &[u8]) -> DataAddress {
         self.data_file.insert(input).address().clone()
     }
 
@@ -65,11 +55,11 @@ impl AvltrieeUpdate<DataAddress, [u8], IdxFileAllocator<DataAddress>> for IdxBin
 impl IdxBinary {
     /// Opens the file and creates the IdxBinary.
     /// # Arguments
-    /// * `path` - Path of file to save data
+    /// * `path` - Path of directory to save data.
     /// * `allocation_lot` - Extends the specified size when the file size becomes insufficient due to data addition.
     /// If you expect to add a lot of data, specifying a larger size will improve performance.
-    pub fn new<P: AsRef<Path>>(path: P, allocation_lot: u32) -> Self {
-        let path = path.as_ref();
+    pub fn new<P: AsRef<Path>>(directory: P, allocation_lot: u32) -> Self {
+        let path = directory.as_ref();
         Self {
             index: IdxFile::new(
                 {
@@ -87,8 +77,34 @@ impl IdxBinary {
         }
     }
 
-    /// Returns the value of the specified row. Returns None if the row does not exist.
-    pub fn bytes(&self, row: NonZeroU32) -> Option<&[u8]> {
-        self.index.get(row).map(|v| self.data_file.bytes(&v))
+    /// Opens the file and creates the IdxBinary.
+    /// /// # Arguments
+    /// * `path` - Path of part of filename without extension to save data.
+    /// * `allocation_lot` - Extends the specified size when the file size becomes insufficient due to data addition.
+    /// If you expect to add a lot of data, specifying a larger size will improve performance.
+    pub fn new_ext<P: AsRef<Path>>(path: P, allocation_lot: u32) -> Self {
+        let path = path.as_ref();
+        Self {
+            index: IdxFile::new(path.with_extension("i"), allocation_lot),
+            data_file: VariousDataFile::new(path.with_extension("d")),
+        }
+    }
+
+    /// Finds a sequence of bytes, inserts it if it doesn't exist, and returns a row.
+    pub fn row_or_insert(&mut self, content: &[u8]) -> NonZeroU32 {
+        let found = search::edge(self, content);
+        if let (Ordering::Equal, Some(found_row)) = (found.ord, found.row) {
+            found_row
+        } else {
+            let row = unsafe { NonZeroU32::new_unchecked(self.index.rows_count() + 1) };
+            unsafe {
+                self.index.insert_unique_unchecked(
+                    row,
+                    self.data_file.insert(content).address().clone(),
+                    found,
+                );
+            }
+            row
+        }
     }
 }
