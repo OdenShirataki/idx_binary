77185931d0f744bb7916e0d4ec17d437ab0c19c5
OdenShirataki <oden.srtk@gmail.com>
2024-02-13 20:03:34 +0900

    refactor: update idx_file

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-mod compare;
+use std::{
+    cmp::Ordering::{self, Equal, Greater, Less},
+    num::NonZeroU32,
+    path::Path,
+};
 
-use std::{cmp::Ordering, num::NonZeroU32, path::Path};
-
-pub use compare::compare;
 pub use idx_file::{
     AvltrieeIter, AvltrieeSearch, AvltrieeUpdate, FileMmap, IdxFile, IdxFileAvlTriee,
 };
@@ -31,11 +32,101 @@ impl AsMut<IdxBinaryAvltriee> for IdxBinary {
 }
 
 impl AvltrieeSearch<DataAddress, [u8], IdxBinaryAllocator> for IdxBinary {
-    fn cmp(&self, left: &DataAddress, right: &[u8]) -> Ordering {
-        compare(self.data_file.bytes(left), right)
+    fn cmp(left: &[u8], right: &[u8]) -> Ordering {
+        let mut left = left.into_iter().fuse();
+        let mut right = right.into_iter().fuse();
+
+        let mut l;
+        let mut r;
+        let mut ll;
+        let mut rr;
+
+        macro_rules! to_digit {
+            ($v:expr) => {
+                $v.and_then(|v| {
+                    let v = *v as isize;
+                    (v >= ('0' as isize) && v <= ('9' as isize)).then_some(v - 48)
+                })
+            };
+        }
+
+        macro_rules! read_left {
+            () => {{
+                l = left.next();
+                ll = to_digit!(l);
+            }};
+        }
+
+        macro_rules! read_right {
+            () => {{
+                r = right.next();
+                rr = to_digit!(r);
+            }};
+        }
+
+        macro_rules! return_unless_equal {
+            ($ord:expr) => {
+                match $ord {
+                    Equal => {}
+                    lastcmp => return lastcmp,
+                }
+            };
+        }
+
+        read_left!();
+        read_right!();
+        'nondigits: loop {
+            match (l, r) {
+                (Some(l_), Some(r_)) => match (ll, rr) {
+                    (Some(ll_), Some(rr_)) => {
+                        if ll_ == 0 || rr_ == 0 {
+                            // left-aligned matching. (`015` < `12`)
+                            return_unless_equal!(ll_.cmp(&rr_));
+                            'digits_left: loop {
+                                read_left!();
+                                read_right!();
+                                match (ll, rr) {
+                                    (Some(ll_), Some(rr_)) => return_unless_equal!(ll_.cmp(&rr_)),
+                                    (Some(_), None) => return Greater,
+                                    (None, Some(_)) => return Less,
+                                    (None, None) => break 'digits_left,
+                                }
+                            }
+                        } else {
+                            // right-aligned matching. (`15` < `123`)
+                            let mut lastcmp = ll_.cmp(&rr_);
+                            'digits_right: loop {
+                                read_left!();
+                                read_right!();
+                                match (ll, rr) {
+                                    (Some(ll_), Some(rr_)) => {
+                                        // `lastcmp` is only used when there are the same number of
+                                        // digits, so we only update it.
+                                        if lastcmp == Equal {
+                                            lastcmp = ll_.cmp(&rr_);
+                                        }
+                                    }
+                                    (Some(_), None) => return Greater,
+                                    (None, Some(_)) => return Less,
+                                    (None, None) => break 'digits_right,
+                                }
+                            }
+                            return_unless_equal!(lastcmp);
+                        }
+                        continue 'nondigits; // do not read from the iterators again
+                    }
+                    (_, _) => return_unless_equal!(l_.cmp(r_)),
+                },
+                (Some(_), None) => return Greater,
+                (None, Some(_)) => return Less,
+                (None, None) => return Equal,
+            }
+            read_left!();
+            read_right!();
+        }
     }
 
-    fn invert<'a, 'b: 'a>(&'a self, value: &'b DataAddress) -> &[u8] {
+    fn invert<'a>(&'a self, value: &'a DataAddress) -> &[u8] {
         self.data_file.bytes(value)
     }
 }
